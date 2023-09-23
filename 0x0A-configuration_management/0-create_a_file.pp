609ee1c4bc0bb624a5221de1c1a7bdfbb7c5d605
Omar Khaled <o.khaled6789@gmail.com>
2023-09-23 04:31:59 +0300

    edited a typo in 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,6 +3,6 @@ file { '/tmp/school':
     ensure  => 'present',
     content => 'I love Puppet',
     owner   => 'www-data',
-    group   => 'w7w-data',
+    group   => 'www-data',
     mode    => '0744'
 }
