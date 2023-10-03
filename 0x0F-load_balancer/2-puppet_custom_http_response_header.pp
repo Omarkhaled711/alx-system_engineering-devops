f67eaff3227f57e086d8f933134425d645b812e3
Omar Khaled <o.khaled6789@gmail.com>
2023-10-03 19:09:02 +0300

    hopefully the checker will work :(

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,17 +1,11 @@
 # Automate the task of creating a custom HTTP header response
-exec { 'update':
-command  => '/usr/bin/apt update',
-provider => shell,
-}
-package { 'nginx':
-ensure  => 'installed',
-require => Exec['update']
-}
+
+Exec { path => '/bin/:/sbin/:/usr/bin/:/usr/sbin/' }
 exec { 'add_header':
-  command  => "sed -i '/listen 80 default_server;/a add_header X-Served-By ${hostname};' /etc/nginx/sites-available/default;",
-  provider => shell,
-}
-exec { 'restart':
-command  => '/usr/sbin/service nginx restart',
-provider => shell,
+    command  => "
+    apt update;
+    apt install nginx -y;
+    sed -i '/http {/a server_tokens off;\\n   add_header X-Served-By ${hostname};' /etc/nginx/nginx.conf
+    service nginx restart;",
+    provider => shell,
 }
